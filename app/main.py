c59bd6f9850fa244c33336d54e14353d2d3c0530
ahmdshrf <ahmd_sherif@outlook.com>
2026-03-20 20:07:13 +0200

    Return any instead of dictonary

--- app/main.py
+++ app/main.py
@@ -78,10 +78,8 @@ def create_shipment(weight : float, destination : str, data : dict[str, Any]) ->
     }
 
 @app.get("/shipments/{field}")
-def get_shipments_fields(field : str, id : int) -> dict[str, Any]:
-    return {
-        field : shipments[id][field]
-    }
+def get_shipments_fields(field : str, id : int) -> Any:
+    return shipments[id][field]
 
 @app.get("/scalar", include_in_schema=False)
 def get_scalar_docs():
