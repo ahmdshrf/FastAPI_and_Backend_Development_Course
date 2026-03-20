46bf9d171ac2e6534ed5e6037ea3af094c8897af
ahmdshrf <ahmd_sherif@outlook.com>
2026-03-20 09:18:23 +0200

    Reorder the latest endpoint

--- app/main.py
+++ app/main.py
@@ -3,6 +3,16 @@ from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
 app = FastAPI()
 
+@app.get("/shipment/latest")
+def get_shipment_latest():
+    return {
+            "id" : 123456,
+            "content" : "table",
+            "weight" : .78,
+            "destination" : "Paris"
+            }
+
+
 @app.get("/shipment/{id}")
 def get_shipment(id : int) -> dict[str, Any]:
     return {
@@ -12,14 +22,7 @@ def get_shipment(id : int) -> dict[str, Any]:
             "destination" : "New York"
             }
 
-@app.get("/shipment/latest")
-def get_shipment_latest():
-    return {
-            "id" : 123456,
-            "content" : "table",
-            "weight" : .78,
-            "destination" : "Paris"
-            }
+
 
 @app.get("/scalar", include_in_schema=False)
 def get_scalar_docs():
