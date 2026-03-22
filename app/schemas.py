6686ebc395f4ca00f0fa034acdc045f38948cf67
ahmdshrf <ahmd_sherif@outlook.com>
2026-03-22 04:45:24 +0200

    Refactor shipment_status field in Shipment model to use ShipmentStatus enum

--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,19 @@
+from enum import Enum
 from random import randint
 from pydantic import BaseModel,Field
 
 def random_zip_code():
     return randint(11000, 11999)
+
+class ShipmentStatus(str, Enum):
+    PENDING = "pending"
+    IN_TRANSIT = "in_transit"
+    DELIVERED = "delivered"
+    PLACED = "placed"
+
 class Shipment(BaseModel):
     content: str = Field(max_length=50, description="Description of the shipment content")
     weight: float = Field(gt=0, le=25, description="Weight must be a positive number and not exceed 25 kg")
     destination: str = Field(max_length=100, description="Destination of the shipment")
-    shipment_status: str = Field(max_length=20, description="Status of the shipment")
+    shipment_status: ShipmentStatus = Field(description="Status of the shipment")
     zip_code: int | None = Field(default_factory=random_zip_code, description="it will be generated randomly if not provided")
