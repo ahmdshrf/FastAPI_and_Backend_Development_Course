0d2d57d8020d3170d2808694b7513442a2ba8595
ahmdshrf <ahmd_sherif@outlook.com>
2026-03-23 04:50:58 +0200

    Refactor Database class to encapsulate shipment operations and improve structure

--- app/database.py
+++ app/database.py
@@ -1,24 +1,115 @@
 import sqlite3
+from typing import Any
+from app.schemas import ShipmentCreate, ShipmentUpdate
 
 
-#Make a Connection to the database
-connection = sqlite3.connect("sqlite.db")
-cursor = connection.cursor()
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect("sqlite.db")
+        self.cur = self.conn.cursor()
+        # Create a table
+        self.create_table()
 
-#1. Create a table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS shipment (
-    id INTEGER PRIMARY KEY,
-    content TEXT,
-    weight REAL,
-    destination TEXT,
-    shipment_status TEXT,
-    zip_code INTEGER
-)""")
+    def create_table(self):
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS shipment (
+            id INTEGER PRIMARY KEY,
+            content TEXT,
+            weight REAL,
+            destination TEXT,
+            shipment_status TEXT,
+            zip_code INTEGER
+        )""")
+        self.conn.commit()
 
-#2. Add shipment data
+    def create_shipment(self, shipment: ShipmentCreate) -> int | None:
+        self.cur.execute("""select max(id) from shipment""")
+        max_id = self.cur.fetchone()
+        new_id = max_id[0] + 1 if max_id[0] is not None else None
+        self.cur.execute(
+            """
+            INSERT INTO shipment
+            VALUES (:id, :content, :weight, :destination, :shipment_status, :zip_code)
+        """,
+            {
+                "id": new_id,
+                "content": shipment.content,
+                "weight": shipment.weight,
+                "destination": shipment.destination,
+                "shipment_status": "placed",
+                "zip_code": shipment.zip_code,
+            },
+        )
+        self.conn.commit()
+        return new_id
+
+    def get_shipment(self, id: int) -> dict[str, Any] | None:
+        self.cur.execute("SELECT * FROM shipment WHERE id = ?", (id,))
+        row = self.cur.fetchone()
+        if row:
+            return {
+                "id": row[0],
+                "content": row[1],
+                "weight": row[2],
+                "destination": row[3],
+                "shipment_status": row[4],
+                "zip_code": row[5],
+            }
+        return None
+    
+    def get_latest_shipment(self) -> dict[str, Any] | None:
+        self.cur.execute("SELECT * FROM shipment ORDER BY id DESC LIMIT 1")
+        row = self.cur.fetchone()
+        if row:
+            return {
+                "id": row[0],
+                "content": row[1],
+                "weight": row[2],
+                "destination": row[3],
+                "shipment_status": row[4],
+                "zip_code": row[5],
+            }
+        return None
+
+    def update_shipment(
+        self, id: int, shipment: ShipmentUpdate
+    ) -> dict[str, Any] | None:
+        self.cur.execute(
+            """
+            UPDATE shipment 
+            SET shipment_status = :shipment_status 
+            SET CONTENT = :content
+            SET weight = :weight
+            SET destination = :destination
+            SET zip_code = :zip_code
+            WHERE id = :id
+            """,
+            {
+                "shipment_status": shipment.shipment_status,
+                "content": shipment.content,
+                "weight": shipment.weight,
+                "destination": shipment.destination,
+                "zip_code": shipment.zip_code,
+                "id": id
+            },
+        )
+        self.conn.commit()
+        return self.get_shipment(id)
+    
+    def delete_shipment(self, id: int) :
+        self.cur.execute("DELETE FROM shipment WHERE id = ?", (id,))
+        self.conn.commit()
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+            self.cursor = None
+
+
+# 2. Add shipment data
 # cursor.execute("""
-#     INSERT INTO shipment 
+#     INSERT INTO shipment
 #     VALUES
 #     (12078, 'table', 0.78, 'Paris', 'in_transit', 75001),
 #     (12079, 'chair', 0.5, 'London', 'delivered', 12345),
@@ -30,29 +121,11 @@ cursor.execute("""
 # """)
 # connection.commit()
 
-#3. Read a shipment by ID
+# 3. Read a shipment by ID
 # cursor.execute("SELECT * FROM shipment WHERE id = 12078")
 # result = cursor.fetchall()
 # print(result)
 
-#4. Delete a shipment by ID
-# cursor.execute("DELETE FROM shipment WHERE id = 12084")
-# connection.commit()
-
-#5. Update a shipment
-id = 12078
-shipment_status = "out_for_delivery"
-cursor.execute("""
-    UPDATE shipment 
-    SET shipment_status = :shipment_status 
-    WHERE id = :id
-    """, {"shipment_status": shipment_status, "id": id})
-connection.commit()  
-
-#6. Delete a table
+# 6. Delete a table
 # cursor.execute("DROP TABLE shipment")
 # connection.commit()
-
-connection.close()
-
-    
